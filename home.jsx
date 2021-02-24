e5daa39b6fe6086a3c496961d45d470cf726f51c
Tyler Trout <tiller1010@gmail.com>
2021-02-23 20:56:59 -0500

    Recent video slider

--- home.jsx
+++ home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { Text, View, TextInput, Button, Image, ScrollView } from 'react-native';
+import { Video } from 'expo-av';
 import Styles from './Styles.js';
 
 class Home extends React.Component {
@@ -8,11 +9,22 @@ class Home extends React.Component {
 		super(props);
 		this.state = {
 			name: '',
-			keywords: ''
+			keywords: '',
+			recentVideos: []
 		}
 	}
 
 	async componentDidMount(){
+		// Get recent videos
+		console.log('Fetching from:', process.env.APP_SERVER_URL);
+		axios.get(`${process.env.APP_SERVER_URL}/recent-videos`)
+			.then(res => {
+				console.log(res)
+				this.setState({
+					recentVideos: res.data.videos
+				});
+			});
+
 		console.log('Fetching from:', process.env.STRAPI_URL);
 		axios.get(`${process.env.STRAPI_URL}/levels`)
 			.then(res => {
@@ -50,24 +62,57 @@ class Home extends React.Component {
 	}
 
 	render(){
+		
+		var apiBaseURL = process.env.APP_SERVER_URL;
+
 		return(
 			<ScrollView>
-				<Text style={Styles.heading}>Video Submissions</Text>
-				<View>
-					<Text style={Styles.subHeading}>Search Terms</Text>
-					<TextInput type="text" onChangeText={(text) => this.setState({keywords: text})} value={this.state.keywords}
-						style={{
-							borderWidth: 1,
-							borderColor: 'black'
-						}}
-					/>
-					<Button title="Search"  onPress={() =>
-						this.props.navigation.navigate('VideosIndex', {keywords: this.state.keywords})
+				<View style={Styles.pad}>
+					<Text>Let's enjoy your</Text>
+					<Text style={Styles.heading}>User Submissions</Text>
+					<View>
+						<Text style={Styles.subHeading}>Search video submissions</Text>
+						<TextInput type="text" onChangeText={(text) => this.setState({keywords: text})} value={this.state.keywords}
+							style={{
+								borderWidth: 1,
+								borderColor: 'black'
+							}}
+						/>
+						<Button title="Search"  onPress={() =>
+							this.props.navigation.navigate('VideosIndex', {keywords: this.state.keywords})
+						}/>
+					</View>
+				    <Button title="View all videos" onPress={() =>
+						this.props.navigation.navigate('VideosIndex')
 					}/>
 				</View>
-			    <Button title="View all videos" onPress={() =>
-					this.props.navigation.navigate('VideosIndex')
-				}/>
+
+				<ScrollView horizontal contentContainerStyle={{ width: 2000 }}>
+			    	{this.state.recentVideos.map((video) => 
+			    		<View key={video._id} style={{height: 300, width: 400}}>
+				    		<View style={{...Styles.pad}}>
+								<Text>{video.title}</Text>
+								<Video
+									posterSource={{uri: video.thumbnailSrc ? apiBaseURL + '/' + video.thumbnailSrc : apiBaseURL + '/images/videoPlaceholder.png'}}
+									ref={(ref) => {
+										if(ref){
+											ref.loadAsync({uri: apiBaseURL + '/' + video.src})
+												.then(() => {
+													ref.setState({
+														showPoster: false
+													});
+												});
+										}
+									}}
+									style={{height: 225, width: '100%'}}
+									usePoster={true}
+									useNativeControls={true}
+									// overrideFileExtensionAndroid="mp4"
+								/>
+							</View>
+						</View>
+		    		)}
+				</ScrollView>
 
 			    {this.state.levels ?
 			    	this.state.levels.map((level) => 
